2dc103d7ac797abe43cdf9bfc3884dd870c21a7f
karma1707 <myzedd1@yandex.ru>
2023-04-23 12:09:44 +0700

    TanStack

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -6,9 +6,9 @@ import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 export const SignIn = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const [error, setError] = useState(false)
+  const [error, setError] = useState(false);
 
   const initialValues = {
     email: "",
@@ -20,12 +20,12 @@ export const SignIn = () => {
     const responce = await res.json();
     // console.log(response)
 
-    if(res.ok){
-      localStorage.setItem('token_auth', responce.token)
-      return navigate('/products')
+    if (res.ok) {
+      localStorage.setItem("token_auth", responce.token);
+      return navigate("/products");
     }
-    
-    return setError(responce.message)
+
+    return setError(responce.message);
   };
 
   const SignInSchema = Yup.object().shape({
@@ -35,31 +35,37 @@ export const SignIn = () => {
 
   return (
     <>
-      <h1>SignIn</h1>
-      <Formik
-        initialValues={initialValues}
-        onSubmit={onSubmit}
-        validationSchema={SignInSchema}
-      >
-        <Form>
-          <div>
-            <Field name="email" placeholder="jane@acme.com*" type="email" />
-            <ErrorMessage name="email" component="p" className={style.error} />
-          </div>
+      <div className={style.signInWindow}>
+        <h1>SignIn</h1>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={onSubmit}
+          validationSchema={SignInSchema}
+        >
+          <Form>
+            <div className={style.signInInformation}>
+              <Field name="email" placeholder="jane@acme.com*" type="email" />
+              <ErrorMessage
+                name="email"
+                component="p"
+                className={style.error}
+              />
+            </div>
 
-          <div>
-            <Field name="password" placeholder="Пароль*" type="password" />
-            <ErrorMessage
-              name="password"
-              component="p"
-              className={style.error}
-            />
-          </div>
+            <div className={style.signInInformation}>
+              <Field name="password" placeholder="Пароль*" type="password" />
+              <ErrorMessage
+                name="password"
+                component="p"
+                className={style.error}
+              />
+            </div>
 
-          <button type="submit">Подтвердить</button>
-          {error && <p className={style.error}>{error}</p>}
-        </Form>
-      </Formik>
+            <button type="submit">Подтвердить</button>
+            {error && <p className={style.error}>{error}</p>}
+          </Form>
+        </Formik>
+      </div>
     </>
   );
 };
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -6,8 +6,7 @@ import { signUpFetch } from "../../api/signup";
 import { useNavigate } from "react-router-dom";
 
 export const SignUp = () => {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const [error, setError] = useState(false);
 
@@ -30,46 +29,56 @@ export const SignUp = () => {
     const res = await signUpFetch(values);
     const responce = await res.json();
 
-    if(res.ok){
-      localStorage.setItem('token_auth', responce.token)
-      return navigate('/products')
+    if (res.ok) {
+      localStorage.setItem("token_auth", responce.token);
+      return navigate("/products");
     }
 
-    return setError(responce.message)
+    return setError(responce.message);
   };
 
   return (
     <>
-      <h1>SignUP</h1>
-      <Formik
-        initialValues={initialValues}
-        onSubmit={onSubmit}
-        validationSchema={SignUpSchema}
-      >
-        <Form>
-          <div>
-            <Field name="email" placeholder="jane@acme.com*" type="email" />
-            <ErrorMessage name="email" component="p" className={style.error} />
-          </div>
+      <div className={style.signUpWindow}>
+        <h1>SignUP</h1>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={onSubmit}
+          validationSchema={SignUpSchema}
+        >
+          <Form>
+            <div className={style.signUpInforamition}>
+              <Field name="email" placeholder="jane@acme.com*" type="email" />
+              <ErrorMessage
+                name="email"
+                component="p"
+                className={style.error}
+              />
+            </div>
 
-          <div>
-            <Field name="password" placeholder="Пароль*" type="password" />
-            <ErrorMessage
-              name="password"
-              component="p"
-              className={style.error}
-            />
-          </div>
+            <div className={style.signUpInforamition}>
+              <Field name="password" placeholder="Пароль*" type="password" />
+              <ErrorMessage
+                name="password"
+                component="p"
+                className={style.error}
+              />
+            </div>
 
-          <div>
-            <Field name="group" placeholder="Группа*" />
-            <ErrorMessage name="group" component="p" className={style.error} />
-          </div>
+            <div className={style.signUpInforamition}>
+              <Field name="group" placeholder="Группа*" />
+              <ErrorMessage
+                name="group"
+                component="p"
+                className={style.error}
+              />
+            </div>
 
-          <button type="submit">Подтвердить</button>
-          {error && <p className={style.error}>{error}</p>}
-        </Form>
-      </Formik>
+            <button type="submit">Подтвердить</button>
+            {error && <p className={style.error}>{error}</p>}
+          </Form>
+        </Formik>
+      </div>
     </>
   );
 };
